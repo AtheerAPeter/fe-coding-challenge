a8778d66756c8010a8d2850260a093e531cecdd0
AtheerAPeter <atheer07713991785@gmail.com>
2025-05-07 15:05:34 +0300

    Add HTML rewrites to next.config.ts for path migration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
-import type {NextConfig} from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
+  /* config options here */
+  async rewrites() {
+    return [
+      {
+        //  to accommodate for path.html migration
+        source: "/:path(.+?)\\.html",
+        destination: "/:path",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
